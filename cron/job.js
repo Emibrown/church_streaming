574dc5101828e398fd289db37436e4427f92e18b
Emibrown <emekaekene@gmail.com>
2020-10-12 13:51:13 +0100

    New update

--- cron/job.js
+++ cron/job.js
@@ -7,7 +7,7 @@ const job = new CronJob('*/5 * * * * *', function () {
     .get('http://127.0.0.1:3000/admin/ready', function (error, response, body) {
         let live_stream = JSON.parse(body).live_stream;
         if(live_stream){
-            // helpers.startStreaming(live_stream)
+            helpers.startStreaming(live_stream)
         }
     });
 }, null, true);
