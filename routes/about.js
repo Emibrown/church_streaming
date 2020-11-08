7cfc27e7922de8a3e1466623adb46c6eadbc9719
Ayah206 <ayahluke20@gmail.com>
2020-11-08 18:47:40 +0100

    update

--- routes/about.js
+++ routes/about.js
@@ -45,27 +45,27 @@ router.get('/', ensureAuthenticated, async (req, res, next) => {
 router.post('/', multers.upload.single('file'), async (req, res, next) => {
     // submit about
     try {
-        if(!req.file){
+        if(!req.body.file){
             sendJSONresponse(res, 400, {"message": "Image required"});
         }
 
-        req.body.image = path.basename(req.file.filename, path.extname(req.file.filename))+'.webp'
+        req.body.image = path.basename(req.body.file.filename, path.extname(req.body.file.filename))+'.webp'
         
-        await sharp(req.file.path)
+        await sharp(req.body.file.path)
         .resize({ width: 384, height: 216 })
         .webp({quality: 60})
         .toFile(path.resolve('./public','small_images',req.body.image))
 
-        await sharp(req.file.path)
+        await sharp(req.body.file.path)
         .resize({ width: 640, height: 360 })
         .webp({quality: 90})
         .toFile(path.resolve('./public','large_images',req.body.image))
 
-        fs.unlinkSync(req.file.path)
+        fs.unlinkSync(req.body.file.path)
         
         const about = new About(req.body);
         await about.save();
-
+        console.log(about)
 
         sendJSONresponse(res, 200, {message: "About added successfully"});
         
