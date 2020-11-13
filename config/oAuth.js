d489eeff4ede77afa479d754ebc3a54da9725631
emmabase <emmaeneche@gmail.com>
2020-11-13 02:23:17 +0100

    oauth

--- config/oAuth.js
+++ config/oAuth.js
@@ -2,7 +2,7 @@ module.exports = {
      facebookOAuth: {
         clientID: "405464257485038",
         clientSecret: "3c06bc39d959b79afac37f6f8dd42719",
-        callbackURL: "http://localhost:3000/facebook/callback",
+        callbackURL: "https://www.faithtofaithtv.org/facebook/callback",
 
      },
      googleOAuth:{
