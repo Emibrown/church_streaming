a0642833cbc2d4a2b359feeb702aae5306f0098b
Emibrown <emekaekene@gmail.com>
2020-11-18 02:52:21 +0100

    Update

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -27,7 +27,7 @@ const youtube = (fileName,stream_key) =>  [
     '-re', '-i', 'http://127.0.0.1:3000/uploads/'+fileName,
     '-c:v', 'libx264', '-preset', 
     'veryfast', '-b:v',
-    '1500k', '-c:a', 'copy',
+    '500k', '-c:a', 'copy',
     '-strict', '-2', '-flags', '+global_header', '-bsf:a', 
     'aac_adtstoasc', '-bufsize', '500k', '-f', 'flv', 'rtmp://a.rtmp.youtube.com/live2/'+stream_key
 ]
